c74b949288985c27741867602cd4bac4a563276e
Dom Udall <dom@synthmedia.co.uk>
2011-11-14 14:54:18 +0000

    Adding license stuff to bootstrap and autoload files

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -1,8 +1,14 @@
 <?php
 
 /**
+ * This file is part of the Synth Notification Bundle.
  *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @author Dom Udall <dom@synthmedia.co.uk>
  */
+
 if (file_exists($file = __DIR__.'/autoload.php')) {
     require_once $file;
 } elseif (file_exists($file = __DIR__.'/autoload.php.dist')) {
