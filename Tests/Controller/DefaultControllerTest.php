00a4a9f8c97ba340fa4f212e3fd9dbb6e5295cad
Dom Udall <dom@synthmedia.co.uk>
2011-11-14 13:15:03 +0000

    Adding travis testing set up, and fake test to test travis set up

--- Tests/Controller/DefaultControllerTest.php
+++ Tests/Controller/DefaultControllerTest.php
@@ -11,16 +11,10 @@
 
 namespace Synth\NotificationBundle\Tests\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
-
-class DefaultControllerTest extends WebTestCase
+class DefaultControllerTest extends \PHPUnit_Framework_TestCase
 {
     public function testIndex()
     {
-        $client = $this->createClient();
-
-        $crawler = $client->request('GET', '/hello/Fabien');
-
-        $this->assertTrue($crawler->filter('html:contains("Hello Fabien")')->count() > 0);
+        $this->assertTrue(1 > 0);
     }
 }
